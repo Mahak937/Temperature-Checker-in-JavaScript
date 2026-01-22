b154d996242c09e52026aadcaafa6ba88abc2f42
Mahak Shrivastava <mahakshrivastava134@gmail.com>
2026-01-23 01:33:30 +0530

    Add files via upload

---
+++ practice-14/app.js
@@ -0,0 +1,11 @@
+let temp = 80;
+
+if(temp >= 40){
+    console.log("temprature is very hot!");
+} else if(temp >= 30){
+    console.log("temprature is hot!");
+} else if(temp >= 20){
+    console.log("temprature is normal!");
+} else{
+    console.log("temprature is cold!");
+}
